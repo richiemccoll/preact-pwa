3eedb2a1eff444dde3ca0b5e1bfd263390513296
MCCO9995 <richard.mccoll@pacificmags.com.au>
2017-05-22 14:42:14 +1000

    Added routing to home and dashboard to bottom navigation

--- components/bottomNav/index.js
+++ components/bottomNav/index.js
@@ -5,17 +5,53 @@ import home from "./home.svg";
 import dashboard from "./dashboard.svg";
 
 export default class BottomNav extends Component {
+
   render() {
+    const items = [
+      {
+        itemUrl: "/",
+        imageSrc: home,
+        imageAlt: "User Home",
+        spanText: "Home"
+      },
+      {
+        itemUrl: "/dashboard",
+        imageSrc: dashboard,
+        imageAlt: "User Dashboard",
+        spanText: "Dashboard"
+      }
+    ];
+
+    const navItems = items.map(i => {
+      return (
+        <NavItem
+          setActive={this.setActive}
+          itemUrl={i.itemUrl}
+          imageSrc={i.imageSrc}
+          imageAlt={i.imageAlt}
+          spanText={i.spanText}
+        />
+      );
+    });
+
     return (
       <div class={style.BottomNavContainer}>
         <nav>
           <ul class={style.BottomNavItems}>
-            <li><img src={home} alt="User Home" /><span>Home</span></li>
-            <li><img src={dashboard} alt="User Dashboard"/><span>Dashboard</span></li>
-            <li>Current</li>
+            {navItems}
           </ul>
         </nav>
       </div>
     );
   }
 }
+
+const NavItem = ({ itemUrl, imageSrc, imageAlt, spanText }) => {
+  return (
+    <Link href={itemUrl}>
+      <li>
+        <img src={imageSrc} alt={imageAlt} /><span>{spanText}</span>
+      </li>
+    </Link>
+  );
+};
